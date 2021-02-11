07badd60f647e18fbf9f5914363b93d4ce2214db
mibreit-photo <Michael.Breitung@mibreit-photo.com>
2021-02-11 08:59:04 +0100

    medium: switched npm module to use es6 - support for tree shaking in consuming code

--- lib/interfaces/IInputValidation.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @author Michael Breitung
- * @copyright Michael Breitung Photography (www.mibreit-photo.com)
- */
-export default interface IInputValidation {
-    validate(): boolean;
-    getLastError(): string | null;
-}
